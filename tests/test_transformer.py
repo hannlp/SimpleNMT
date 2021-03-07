230c1c65129c35a9de6d26fe2487a7a121b536e0
Yuchen Han <42163912+hannlp@users.noreply.github.com>
2021-03-07 19:54:38 +0800

    add test_transformer.py

---
+++ tests/test_transformer.py
@@ -0,0 +1,21 @@
+import sys
+import os
+print(os.path, os.getcwd())
+sys.path.append(os.getcwd())
+import torch
+from simplenmt.models.transformer import Transformer
+
+n_src_words = 20
+n_tgt_words = 25
+batch_size = 5
+src_len = 64
+tgt_len = 60
+
+model = Transformer(n_src_words=n_src_words, n_tgt_words=n_tgt_words)
+print(model)
+
+src_tokens = torch.randint(n_src_words, (batch_size, src_len))
+prev_output_tokens = torch.randint(n_tgt_words, (batch_size, tgt_len))
+
+out = model(src_tokens, prev_output_tokens)
+print(out.shape)
