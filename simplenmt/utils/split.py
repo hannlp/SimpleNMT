a529d208d0beab65826ebd38ba2204a841ccc639
Yuchen Han <42163912+hannlp@users.noreply.github.com>
2021-03-29 22:23:46 +0800

    Update split.py

--- simplenmt/utils/split.py
+++ simplenmt/utils/split.py
@@ -28,14 +28,8 @@ def split(src_fpath, tgt_fpath, nsrc='zh', ntgt='en', ratio=(0.9, 0.05, 0.05), n
         src_val.write(s)
         tgt_val.write(t)
   
-  src_fp.close()
-  tgt_fp.close()
-  src_train.close()
-  src_test.close()
-  src_val.close()
-  tgt_train.close()
-  tgt_test.close()
-  tgt_val.close()
+  src_fp.close(); tgt_fp.close(); src_train.close(); src_test.close()
+  src_val.close(); tgt_train.close(); tgt_test.close(); tgt_val.close()
 
 if __name__ == '__main__':      
     split(src_fpath=sys.argv[1], tgt_fpath=sys.argv[2], nsrc='zh', ntgt='en', ratio=(0.95, 0.025, 0.025), new_data_dir=sys.argv[3])
