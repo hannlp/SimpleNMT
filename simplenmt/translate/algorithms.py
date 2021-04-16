b766220e7733ddf7c5a34486aa061d9c38985fbd
Yuchen Han <42163912+hannlp@users.noreply.github.com>
2021-04-16 09:52:47 +0800

    Update algorithms.py

--- simplenmt/translate/algorithms.py
+++ simplenmt/translate/algorithms.py
@@ -32,18 +32,18 @@ def greedy_search(self, src_tokens, f_enc, f_dec, device):
     
     return gen_seqs
 
-def beam_search(self, src_tokens, beam_width=4):
+def beam_search(self, src_tokens, beam_size=4):
     # init
     bos = -1
     MAX_LENGTH = 512
     batch_size = src_tokens.size(0)
-    
-    # - gen_seqs: (B x beam_width, step)
-    gen_seqs = torch.full((batch_size * beam_width, 1), bos)
-    done = torch.tensor([False] * (batch_size * beam_width))
+
+    # - gen_seqs: (B x beam_size, step)
+    gen_seqs = torch.full((batch_size * beam_size, 1), bos)
+    done = torch.tensor([False] * (batch_size * beam_size))
     best_scores = torch.full([batch_size], -1e10)
-    _beam_offset = torch.arange(0, batch_size * beam_width, step=beam_width)
-    topk_log_probs = torch.tensor([0.0] + [float("-inf")] * (beam_width - 1)).repeat(self.batch_size)
+    _beam_offset = torch.arange(0, batch_size * beam_size, step=beam_size)
+    topk_log_probs = torch.tensor([0.0] + [float("-inf")] * (beam_size - 1)).repeat(self.batch_size)
     
     # buffers for the topk scores and 'backpointer'
     topk_scores = torch.empty((self.batch_size, self.beam_size))
@@ -52,14 +52,34 @@ def beam_search(self, src_tokens, beam_width=4):
 
     encoder_out, src_mask = f_enc(src_tokens) # mask 可以广播，不用repeat
     # - encoder_out: (batch_size, src_len, d_model)
-    encoder_outs = encoder_out.repeat(beam_width, 1, 1)
-    # - encoder_outs: (batch_size * beam_width, src_len, d_model)
+    encoder_outs = encoder_out.repeat(beam_size, 1, 1)
+    # - encoder_outs: (batch_size * beam_size, src_len, d_model)
 
     for step in range(2, MAX_LENGTH):
         
-        decoder_in = gen_seqs[:, -1].view(-1, 1, 1)
+        #decoder_in = gen_seqs[:, -1].view(-1, 1, 1)
+        decoder_in = gen_seqs
 
         # Decoder forward, takes [tgt_len, batch, nfeats] as input
-        decoder_out = f_dec(decoder_in, encoder_outs)
+        model_out = f_dec(decoder_in, encoder_outs, src_mask) # log softmax
+        # - model_out: (batch_size * beam_size, n_tgt_words)
 
+        log_probs = model_out
+
+        B_times_beam, vocab_size = log_probs.shape
+        # using integer division to get an integer _B without casting
+        _B = B_times_beam // beam_size
+
+        # Multiply probs by the beam probability.
+        log_probs += topk_log_probs.view(_B * self.beam_size, 1)
+
+        # May be length penalty
+        curr_scores = log_probs
+
+        # Pick up candidate token by curr_scores
+        # Flatten probs into a list of possibilities.
+        curr_scores = curr_scores.reshape(-1, self.beam_size * vocab_size)
+        topk_scores, topk_ids = torch.topk(curr_scores, self.beam_size, dim=-1)
+
+        
         pass
