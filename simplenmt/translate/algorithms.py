7571c655a848d8b7f36c089865f57c7f32950cee
Yuchen Han <42163912+hannlp@users.noreply.github.com>
2021-04-16 09:21:33 +0800

    Update algorithms.py

--- simplenmt/translate/algorithms.py
+++ simplenmt/translate/algorithms.py
@@ -37,10 +37,18 @@ def beam_search(self, src_tokens, beam_width=4):
     bos = -1
     MAX_LENGTH = 512
     batch_size = src_tokens.size(0)
+    
     # - gen_seqs: (B x beam_width, step)
     gen_seqs = torch.full((batch_size * beam_width, 1), bos)
     done = torch.tensor([False] * (batch_size * beam_width))
-
+    best_scores = torch.full([batch_size], -1e10)
+    _beam_offset = torch.arange(0, batch_size * beam_width, step=beam_width)
+    topk_log_probs = torch.tensor([0.0] + [float("-inf")] * (beam_width - 1)).repeat(self.batch_size)
+    
+    # buffers for the topk scores and 'backpointer'
+    topk_scores = torch.empty((self.batch_size, self.beam_size))
+    topk_ids = torch.empty((self.batch_size, self.beam_size))
+    _batch_index = torch.empty([self.batch_size, self.beam_size])
 
     encoder_out, src_mask = f_enc(src_tokens) # mask 可以广播，不用repeat
     # - encoder_out: (batch_size, src_len, d_model)
