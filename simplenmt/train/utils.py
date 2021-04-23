a5435f15a18c283ba1c1690defe00bc2336bd316
Yuchen Han <42163912+hannlp@users.noreply.github.com>
2021-04-23 21:36:56 +0800

    Update utils.py

--- simplenmt/train/utils.py
+++ simplenmt/train/utils.py
@@ -1,11 +1,15 @@
+import os
 import logging
 
 def get_logger(args):
+    log_path = args.save_path + '/log.txt'
+    if os.path.exists(log_path):
+        os.remove(log_path)
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s | %(message)s', datefmt='%y-%m-%d %H:%M:%S')
 
-    file_handler = logging.FileHandler(args.save_path + '/log.txt')
+    file_handler = logging.FileHandler(log_path)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
     consle_handler = logging.StreamHandler()
