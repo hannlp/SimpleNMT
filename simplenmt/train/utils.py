c8e5203c776a5315287a5fdc126de4991fe6d4ee
Yuchen Han <42163912+hannlp@users.noreply.github.com>
2021-04-23 21:23:27 +0800

    Update utils.py

--- simplenmt/train/utils.py
+++ simplenmt/train/utils.py
@@ -3,19 +3,15 @@ import logging
 def get_logger(args):
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(message)s', datefmt='%y-%m-%d %H:%M:%S')
+    formatter = logging.Formatter('%(asctime)s | %(message)s', datefmt='%y-%m-%d %H:%M:%S')
 
-    # 使用FileHandler输出到文件
-    fh = logging.FileHandler(args.save_path + '/log.txt')
-    fh.setLevel(logging.INFO)
-    fh.setFormatter(formatter)
+    file_handler = logging.FileHandler(args.save_path + '/log.txt')
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    consle_handler = logging.StreamHandler()
+    consle_handler.setLevel(logging.INFO)
+    consle_handler.setFormatter(formatter)
 
-    # 使用StreamHandler输出到屏幕
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    ch.setFormatter(formatter)
-
-    # 添加两个Handler
-    logger.addHandler(ch)
-    logger.addHandler(fh)
+    logger.addHandler(consle_handler)
+    logger.addHandler(file_handler)
     return logger
