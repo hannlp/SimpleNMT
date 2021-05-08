4cd8e9d49841bd0bca069eb74fa614bec2af60ac
Yuchen Han <42163912+hannlp@users.noreply.github.com>
2021-05-08 17:56:58 +0800

    add two transformer version

--- simplenmt/models/__init__.py
+++ simplenmt/models/__init__.py
@@ -7,6 +7,8 @@ from .rnnsearch import RNNSearch
 from .seq2seq import Seq2Seq
 from .luong import Luong
 from .convs2s import ConvS2S
+from .transformer_nope import Transformer_nope
+from .transformer_gelu import Transformer_gelu
 
 '''
 transformer:
@@ -24,7 +26,8 @@ transformer:
 
 '''
 
-str2model = {"Transformer": Transformer, "RNNSearch": RNNSearch, "Seq2Seq": Seq2Seq, "Luong": Luong, "ConvS2S": ConvS2S}
+str2model = {"Transformer": Transformer, "RNNSearch": RNNSearch, "Seq2Seq": Seq2Seq, 
+                "Luong": Luong, "ConvS2S": ConvS2S, "Transformer_nope": Transformer_nope, "Transformer_gelu": Transformer_gelu}
 
 def build_model(args):
 
@@ -40,6 +43,28 @@ def build_model(args):
             'p_drop': args.p_drop,
             'max_seq_len': args.max_seq_len
             }, 
+        "Transformer_nope": {
+            'n_src_words': args.n_src_words,
+            'n_tgt_words': args.n_tgt_words, 
+            'src_pdx': args.src_pdx, 
+            'tgt_pdx': args.tgt_pdx, 
+            'd_model': args.d_model, 
+            'n_head': args.n_head, 
+            'n_layers': args.n_layers, 
+            'p_drop': args.p_drop,
+            'max_seq_len': args.max_seq_len
+            },
+        "Transformer_gelu": {
+            'n_src_words': args.n_src_words,
+            'n_tgt_words': args.n_tgt_words, 
+            'src_pdx': args.src_pdx, 
+            'tgt_pdx': args.tgt_pdx, 
+            'd_model': args.d_model, 
+            'n_head': args.n_head, 
+            'n_layers': args.n_layers, 
+            'p_drop': args.p_drop,
+            'max_seq_len': args.max_seq_len
+            },
         "Luong": {
             'n_src_words': args.n_src_words,
             'n_tgt_words': args.n_tgt_words,
