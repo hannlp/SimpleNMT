9ee6331f065e4310c6346a17e6b12024fd97906b
Yuchen Han <42163912+hannlp@users.noreply.github.com>
2021-05-25 22:46:53 +0800

    Update __init__.py

--- simplenmt/models/__init__.py
+++ simplenmt/models/__init__.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
  # debugging: use bias=False in out_vocab_proj
 #from .transformer_dev import Transformer
-from .transformer_dev import Transformer
+from .transformer import Transformer
 from .rnnsearch import RNNSearch
 from .seq2seq import Seq2Seq
 from .luong import Luong
@@ -35,17 +35,7 @@ str2model = {"Transformer": Transformer, "RNNSearch": RNNSearch, "Seq2Seq": Seq2
 
 
 # old
-# "Transformer": {
-#             'n_src_words': args.n_src_words,
-#             'n_tgt_words': args.n_tgt_words, 
-#             'src_pdx': args.src_pdx, 
-#             'tgt_pdx': args.tgt_pdx, 
-#             'd_model': args.d_model, 
-#             'n_head': args.n_head, 
-#             'n_layers': args.n_layers, 
-#             'p_drop': args.p_drop,
-#             'max_seq_len': args.max_seq_len
-#             }, 
+
 def build_model(args):
 
     model_args = {
@@ -54,15 +44,26 @@ def build_model(args):
             'n_tgt_words': args.n_tgt_words, 
             'src_pdx': args.src_pdx, 
             'tgt_pdx': args.tgt_pdx, 
-            'd_model': args.d_model,
-            'd_ff': args.d_ff, 
+            'd_model': args.d_model, 
             'n_head': args.n_head, 
-            'n_layers': args.n_layers,
-            'n_encoder_layers': args.n_encoder_layers,
-            'n_decoder_layers': args.n_decoder_layers, 
+            'n_layers': args.n_layers, 
             'p_drop': args.p_drop,
             'max_seq_len': args.max_seq_len
             }, 
+        # "Transformer": {
+        #     'n_src_words': args.n_src_words,
+        #     'n_tgt_words': args.n_tgt_words, 
+        #     'src_pdx': args.src_pdx, 
+        #     'tgt_pdx': args.tgt_pdx, 
+        #     'd_model': args.d_model,
+        #     'd_ff': args.d_ff, 
+        #     'n_head': args.n_head, 
+        #     'n_layers': args.n_layers,
+        #     'n_encoder_layers': args.n_encoder_layers,
+        #     'n_decoder_layers': args.n_decoder_layers, 
+        #     'p_drop': args.p_drop,
+        #     'max_seq_len': args.max_seq_len
+        #     }, 
         "Transformer_LSTM": {
             'n_src_words': args.n_src_words,
             'n_tgt_words': args.n_tgt_words, 
